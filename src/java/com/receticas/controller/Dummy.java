149aeaf07c6351ef6b9453b5f8104ededd0f8f9a
orggue <jorgechato1@gmail.com>
2017-03-09 12:09:33 +0100

    force error test

---
+++ src/java/com/receticas/controller/Dummy.java
@@ -0,0 +1,15 @@
+package com.receticas.controller;
+
+/**
+ * =====================================================================================
+ * Filename: Dummy.java
+ * Version: 1.0
+ * Created: 3/9/17
+ * Author: JORGE CHATO (651348)
+ * =====================================================================================
+ */
+public class Dummy {
+    public int sum(int a, int b){
+        return a + b;
+    }
+}
---
+++ src/test/DummyTest.java
@@ -0,0 +1,18 @@
+import com.receticas.controller.Dummy;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+/**
+ * =====================================================================================
+ * Filename: DummyTest.java
+ * Version: 1.0
+ * Created: 3/9/17
+ * Author: JORGE CHATO (651348)
+ * =====================================================================================
+ */
+public class DummyTest {
+    @Test
+    public void sum() throws Exception {
+        Dummy dm = new Dummy();
+        assertEquals(8, dm.sum(2,5));
+    }
+}
